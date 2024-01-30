609ca05b5b64954c9062578e1b6aed006e28427d
umang agarwal <umanggiiitr@gmail.com>
2024-01-30 09:41:00 +0530

    ViewList API Added

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -17,7 +17,7 @@ const storage=multer.diskStorage({
 const upload=multer({storage:storage});
 
 foodRouter.post("/add",upload.single("image"),addFood);
-foodRouter.get("/list",listFood);
 foodRouter.post("/remove",removeFood);
+foodRouter.get("/list",listFood);
 
 export default foodRouter;
